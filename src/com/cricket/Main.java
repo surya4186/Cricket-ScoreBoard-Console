3055cabbde9513514697973c1f687c3c174b5cab
Surya <mjsurya00@gamil.com>
2024-07-03 14:39:41 +0530

    initial commit

--- src/com/cricket/Main.java
+++ src/com/cricket/Main.java
@@ -1,5 +1,9 @@
 package com.cricket;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -12,13 +16,98 @@ public class Main {
 		System.out.println("Enter the team names");
 		teamName[0] = scan.next();
 		teamName[1] = scan.next();
-		System.out.println("Enter team 1 name");
+//		System.out.println("Enter team 1 name");
 		String[] team1Players = getPlayers();
 		String[] team2Players = getPlayers();
 
 		Team team1 = new Team(teamName[0], team1Players);
 		Team team2 = new Team(teamName[1], team2Players);
+		innings(team1, team2);
+		innings(team2, team1);
+		if (team1.totalRuns < team2.totalRuns) {
+			team2.result = "win";
+			team1.result = "lost";
 
+		} else {
+			team1.result = "win";
+			team2.result = "Lost";
+		}
+		System.out.println("First innings");
+		team1.printScore();
+		System.out.println();
+		team1.battingScore();
+		System.out.println();
+
+		team2.printBowlingScoreCard();
+		System.out.println();
+
+		team1.printPlayerStats();
+		System.out.println();
+		System.out.println("second innings");
+		team2.printScore();
+		System.out.println();
+		team2.battingScore();
+		team1.printBowlingScoreCard();
+		team2.printPlayerStats();
+
+	}
+
+	private static void innings(Team battingTeam, Team bowlingTeam) {
+
+		Deque<Integer> batQue = new ArrayDeque<>(Arrays.asList(0, 1));
+		Queue<Integer> bowQue = new ArrayDeque<>(Arrays.asList(0, 1));
+		int curOver = 0;
+		while (curOver < 2 && battingTeam.batsmanOut < 2) {
+			int curBowler = bowQue.peek();
+			int balls = 0;
+			while (balls < 6 && battingTeam.batsmanOut < 2) {
+				String ballResult = scan.next();
+				int curBat = batQue.peek();
+				int runs = ballResult(battingTeam, bowlingTeam, ballResult, curBowler, curBat, batQue);
+				if (runs % 2 == 1) {
+					batQue.add(batQue.poll());
+				}
+
+				balls++;
+
+			}
+			if (battingTeam.batsmanOut < 2) {
+				batQue.add(batQue.poll());
+				bowQue.add(bowQue.poll());
+				battingTeam.over++;
+			}
+
+			bowlingTeam.increseOver(bowlingTeam, curBowler);
+
+			curOver++;
+
+		}
+
+	}
+
+	private static int ballResult(Team battingTeam, Team bowlingTeam, String ballResult, int curBowler, int curBat,
+			Queue<Integer> batQue) {
+		// TODO Auto-generated method stub
+		int run = 0;
+		if (ballResult.equals("W")) {
+			battingTeam.batsmanOut(curBat);
+
+			bowlingTeam.incrementWickets(curBowler);
+			batQue.poll();
+			if (battingTeam.batsmanOut < 2) {
+				((Deque<Integer>) batQue).addFirst(2);
+
+			}
+
+		} else if (ballResult.equals("WD") || ballResult.equals("NB")) {
+			battingTeam.extras();
+
+		} else {
+			run = Integer.parseInt(ballResult);
+			battingTeam.addRun(run, curBat);
+			bowlingTeam.addRuns(curBowler, run);
+		}
+		return run;
 	}
 
 	private static String[] getPlayers() {
--- src/com/cricket/Team.java
+++ src/com/cricket/Team.java
@@ -24,8 +24,8 @@ public class Team {
 		this.totalRuns = 0;
 		this.batsman = new String[3];
 		this.bowler = new String[2];
-		this.batsmanMap = new LinkedHashMap<>();
-		this.bowlerMap = new LinkedHashMap<>();
+		this.batsmanMap = new LinkedHashMap<>(); // [runs, balls, out]
+		this.bowlerMap = new LinkedHashMap<>(); // [overs, balls, runs, wickets]
 		this.batsmanOut = 0;
 		this.result = null;
 		this.over = 0;
@@ -40,9 +40,103 @@ public class Team {
 
 			} else {
 				bowler[i - 3] = palyers[i];
-				bowlerMap.put(bowler[i - 3], new int[2]);
+				bowlerMap.put(bowler[i - 3], new int[4]);
 			}
 		}
+
+	}
+
+	public void addRun(int run, int curBat) {
+		
+		totalRuns += run;
+		batsmanMap.get(batsman[curBat])[0] += run;
+		batsmanMap.get(batsman[curBat])[1]++;
+
+	}
+
+	public void batsmanOut(int curBat) {
+	
+		batsmanOut++;
+		wicket++;
+		batsmanMap.get(batsman[curBat])[2] = 1;
+		batsmanMap.get(batsman[curBat])[1]++;
+
+	}
+
+	public void incrementWickets(int curBowler) {
+		
+		bowlerMap.get(bowler[curBowler])[3]++;
+
+
+	}
+
+	public void addRuns(int bowlerIndex, int runs) {
+		bowlerMap.get(bowler[bowlerIndex])[2] += runs;
+		bowlerMap.get(bowler[bowlerIndex])[1]++;
+	}
+
+	public void extras() {
+
+		extras++;
+
+	}
+
+	public void printScore() {
+		System.out.println(teamName + "---" + totalRuns + "------ " + " wicket :" + wicket);
+	}
+
+	public void battingScore() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 3; i++) {
+			if (batsmanMap.get(batsman[i])[2] != 1) {
+				System.out.println(batsman[i] + "*---runs: " + batsmanMap.get(batsman[i])[0] + "("
+						+ batsmanMap.get(batsman[i])[1] + ")");
+			} else {
+				System.out.println(batsman[i] + "---runs: " + batsmanMap.get(batsman[i])[0] + "("
+						+ batsmanMap.get(batsman[i])[1] + ")");
+
+			}
+		}
+
+	}
+
+	public void printBowlingScoreCard() {
+		for (int i = 0; i < 2; i++) {
+			System.out.println(bowler[i] + " " + bowlerMap.get(bowler[i])[0] + " " + bowlerMap.get(bowler[i])[1] + " "
+					+ bowlerMap.get(bowler[i])[2] + " / " + bowlerMap.get(bowler[i])[3]);
+		}
+	}
+
+	public void printPlayerStats() {
+		for (int i = 0; i < 5; i++) {
+			if (i < 3) {
+				int runs = batsmanMap.get(batsman[i])[0];
+				int balls = batsmanMap.get(batsman[i])[1];
+				float sr = (int) 100 * ((float) runs / balls);
+				float c = (100 * ((float) runs / totalRuns));
+				int srInt = (int) Math.ceil(sr);
+				int cInt = (int) Math.ceil(c);
+				System.out.println(batsman[i] + " - " + runs + "(" + balls + ") " + srInt + "% " + cInt + " " + runs
+						+ "/" + totalRuns);
+			} else {
+				int runs = bowlerMap.get(bowler[i - 3])[2];
+				int wicket = bowlerMap.get(bowler[i - 3])[3];
+				int c = bowlerMap.get(bowler[i - 3])[3] / wicket;
+				String overs = bowlerMap.get(bowler[i - 3])[0] + "" + bowlerMap.get(bowler[i - 3])[1];
+				if (wicket == 0) {
+					System.out.println(
+							bowler[i - 3] + " " + runs + "/" + wicket + " (" + overs + ") " + "W " + wicket + " " + 0);
+				} else {
+					System.out.println(bowler[i - 3] + " " + runs + "/" + wicket + " (" + overs + ") " + "W " + wicket
+							+ " " + (c / wicket) * 100);
+				}
+			}
+		}
+	}
+
+	public void increseOver(Team bowlingTeam, int curBowler) {
+		// TODO Auto-generated method stub
+		bowlingTeam.bowlerMap.get(bowler[curBowler])[0]++;
 
 	}
 
